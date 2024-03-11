e8555d4ccb47e410cc91d41c9c4faf853be047a3
Niall BURDON <niall.burdon@rubrash.com>
2024-03-11 11:34:40 +0100

    removed functions onLeave and enterBack

--- app.vue
+++ app.vue
@@ -472,33 +472,33 @@ onMounted(() => {
           }
         );
       },
-      onLeave: function () {
-        gsap.fromTo(
-          elem,
-          { autoAlpha: 1 },
-          { autoAlpha: 0, overwrite: "auto" }
-        );
-      },
-      onEnterBack: function () {
-        gsap.fromTo(
-          elem,
-          { y: -100, autoAlpha: 0 },
-          {
-            duration: 1.25,
-            y: 0,
-            autoAlpha: 1,
-            ease: "back",
-            overwrite: "auto",
-          }
-        );
-      },
-      onLeaveBack: function () {
-        gsap.fromTo(
-          elem,
-          { autoAlpha: 1 },
-          { autoAlpha: 0, overwrite: "auto" }
-        );
-      },
+      // onLeave: function () {
+      //   gsap.fromTo(
+      //     elem,
+      //     { autoAlpha: 1 },
+      //     { autoAlpha: 0, overwrite: "auto" }
+      //   );
+      // },
+      // onEnterBack: function () {
+      //   gsap.fromTo(
+      //     elem,
+      //     { y: -100, autoAlpha: 0 },
+      //     {
+      //       duration: 1.25,
+      //       y: 0,
+      //       autoAlpha: 1,
+      //       ease: "back",
+      //       overwrite: "auto",
+      //     }
+      //   );
+      // },
+      // onLeaveBack: function () {
+      //   gsap.fromTo(
+      //     elem,
+      //     { autoAlpha: 1 },
+      //     { autoAlpha: 0, overwrite: "auto" }
+      //   );
+      // },
     });
   });
 });
